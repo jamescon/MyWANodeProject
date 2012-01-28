bf25888d70703acc82d737f0905089b9e846c598
James Conard <jamescon@live.com>
2012-01-28 22:07:20 +0000

    adding

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var port = process.env.PORT;
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('hello azure v2\n');
+}).listen(port);
+
